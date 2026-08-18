bf5946eff98776048048e69f86559ca2b8744255
Jia Min <jasymin1108@gmail.com>
2026-08-18 21:40:41 +0800

    Update train_stage2.py

--- train_stage2.py
+++ train_stage2.py
@@ -29,8 +29,7 @@ def get_dataset(args):
         
     trainset = DatasetLoader(dataset_name=args.dataset, phase='train', size=args.image_size)
     valset = DatasetLoader(dataset_name=args.dataset, phase='valid', size=args.image_size)
-    testset = DatasetLoader(dataset_name=args.dataset, phase='test', size=args.image_size)
-    
+ 
     train_sampler = CategoriesSampler(trainset.label, args.train_batch,
                                         args.train_way, args.shot + args.train_query)
     train_loader = DataLoader(dataset=trainset, batch_sampler=train_sampler,
@@ -41,17 +40,12 @@ def get_dataset(args):
     val_loader = DataLoader(dataset=valset, batch_sampler=val_sampler,
                             num_workers=args.worker, pin_memory=True)
     
-    test_sampler = CategoriesSampler(testset.label, args.test_batch,
-                                    args.test_way, args.shot + args.test_query)
-    test_loader = DataLoader(dataset=testset, batch_sampler=test_sampler,
-                            num_workers=args.worker, pin_memory=True)
-    
-    return train_loader, val_loader, test_loader, n_cls
+    return train_loader, val_loader, n_cls
 
 def main(args):
     ensure_path(args.save_path)
 
-    train_loader, val_loader, test_loader, n_cls = get_dataset(args)
+    train_loader, val_loader, n_cls = get_dataset(args)
    
     teacher = CvT(num_classes=n_cls).cuda()
     checkpoint_file = os.path.join(args.stage1_path, 'max-test-acc.pth')
